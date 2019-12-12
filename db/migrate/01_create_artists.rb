b813fd0fce9d5ddef64dd808875ee67ffe9f7860
Daniel Matney <danieljmatney@gmail.com>
2019-12-12 01:49:20 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,14 +1,4 @@
 class CreateArtists < ActiveRecord::Migration[5.2]
-  
-sql = <<-SQL
-  CREATE TABLE IF NOT EXISTS artists (
-  id INTEGER PRIMARY KEY,
-  name TEXT,
-  genre TEXT,
-  age INTEGER,
-  hometown TEXT
-  )
-SQL
  
 ActiveRecord::Base.connection.execute(sql)
 
